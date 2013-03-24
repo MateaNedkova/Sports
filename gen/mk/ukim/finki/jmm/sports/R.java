ee9bdbdc14ece1253fa97989805412798466549a
MateaNedkova <matea.nedkova@gmail.com>
2013-03-24 13:59:04 +0100

    Landscape i portrait
    Kopceto vo aplikacijata e postaveno samo za da go pokaze efektot kako
    pri rotiranje na ekranot ke mu se smeni negovata polozba

--- gen/mk/ukim/finki/jmm/sports/R.java
+++ gen/mk/ukim/finki/jmm/sports/R.java
@@ -24,16 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int image=0x7f080004;
-        public static final int kopce=0x7f080003;
-        public static final int language=0x7f080008;
+        public static final int action_settings=0x7f080008;
+        public static final int image=0x7f080003;
+        public static final int kopce=0x7f080002;
+        public static final int language=0x7f080007;
         public static final int lista=0x7f080001;
         public static final int najava=0x7f080000;
-        public static final int objasnuvanje=0x7f080002;
-        public static final int pager=0x7f080006;
-        public static final int pager_title_strip=0x7f080007;
-        public static final int text=0x7f080005;
+        public static final int pager=0x7f080005;
+        public static final int pager_title_strip=0x7f080006;
+        public static final int text=0x7f080004;
     }
     public static final class layout {
         public static final int activity_sports=0x7f030000;
@@ -47,9 +46,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int btn=0x7f050005;
+        public static final int btn=0x7f050004;
         public static final int language=0x7f050002;
-        public static final int obj=0x7f050004;
         public static final int podnaslov=0x7f050003;
     }
     public static final class style {
